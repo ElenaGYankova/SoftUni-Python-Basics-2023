66f144e57f0d598559f0088f00c3592ba438ae14
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-04-24 10:49:17 +0300

    Delete Exam_prep directory

--- Exam_prep/exam_28_and_29_march_2020/01_birthday_party.py
+++
@@ -1,8 +0,0 @@
-rent = float(input())
-
-cake = rent * 0.20
-drinks = cake * 0.55
-animator = rent / 3
-
-total = rent + cake + drinks + animator
-print(total)
--- Exam_prep/exam_6_and_7_april_2019/01_oscars_ceremony.py
+++
@@ -1,8 +0,0 @@
-rent = int(input())
-
-statuettes = rent - (rent * 0.30)
-catering = statuettes - (statuettes * 0.15)
-sounding = catering / 2
-
-total = rent + statuettes + catering + sounding
-print(f"{total:.2f}")
